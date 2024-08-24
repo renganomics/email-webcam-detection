f14a001781dc4a7bac57318c3e5a4b96b0672f09
renganomics <orengasi@gmail.com>
2024-08-24 21:34:24 +0100

    Day 36: Connected email and implemented threading for send_email and clean_folder functions

--- emailing.py
+++ emailing.py
@@ -1,2 +1,33 @@
-def send_email():
-    print("Email was sent")
+import smtplib
+import os
+import imghdr
+
+from email.message import EmailMessage
+
+PASSWORD = os.environ["PASSWORD"]
+SENDER = "orengasi@gmail.com"
+RECEIVER = "orengasi@gmail.com"
+
+
+def send_email(image_path):
+    print("send_email function started")
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey we just saw a new customer!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image",
+                                 subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+    print("send_email function ended")
+
+
+if __name__ == "__main__":
+    send_email(image_path="images/19.png")
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import cv2
 import time
+import glob
+import os
+
 from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
+
+def clean_folder():
+    print("clean_folder function started")
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+    print("clean_folder function ended")
 
 while True:
     status = 0
@@ -33,20 +46,32 @@ while True:
 
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+
+
+        email_thread.start()
 
     print(status_list)
 
     cv2.imshow("Video", frame)
-
     key = cv2.waitKey(1)
 
     if key == ord("q"):
         break
 
 video.release()
+
+clean_thread = Thread(target=clean_folder())
+clean_thread.daemon = True
+clean_thread.start()
